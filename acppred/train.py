502bb14a7374d58467f46d0adce960fc3112236b
thgbaum <thg.baum@gmail.com>
2022-12-15 15:51:46 -0300

    CLI and setup.py updated

--- acppred/train.py
+++ acppred/train.py
@@ -1,16 +1,45 @@
 from sklearn.ensemble import RandomForestClassifier
 from acppred.models import Model
+from argparse import ArgumentParser
 
 def main():
 
-    model = Model(
-    estimator=RandomForestClassifier(),
-    positive_peptides='data/raw/positive.txt',
-    negative_peptides='data/raw/negative.txt'
+    argument_parser = ArgumentParser(description='Trains a anticancer peptide classification model')
+    argument_parser.add_argument(
+        '--positive-peptides', 
+        default='data/raw/positive.txt', 
+        help='a file containing anticancer peptides'
+        )
+    argument_parser.add_argument(
+        '--negative-peptides',
+        default='data/raw/negative.txt',
+        help='a file containing non-anticancer peptides'
+    )
+    argument_parser.add_argument(
+        '--output',
+        help='path to the output training model',
+        required=True
+    )
+    argument_parser.add_argument(
+        '--show-report',
+        help='Shows the classification report after training',
+        action='store_true'
     )
 
-    model.train()
-    model.save('data/models/model.pickle')
+    arguments = argument_parser.parse_args()
+
+    model = Model(
+    estimator=RandomForestClassifier(),
+    positive_peptides=arguments.positive_peptides,
+    negative_peptides=arguments.negative_peptides
+    )
+
+    report = model.train()
+    
+    if arguments.show_report:
+        print(report)
+
+    model.save(arguments.output)
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name='acppred',
     version='0.0.1', 
     packages=find_packages(),
-    author='thgbaum'
+    author='thgbaum',
+    entry_points = {
+        'console_scripts': [
+            'acppred-train = acppred.train:main',
+            'acppred-predict = acppred.predict:main'
+        ]
+    }
 )
